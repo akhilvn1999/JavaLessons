ca01f9460cff2b4749670f0f7f3b1af1f896e098
akhilvn1999 <akhilvnair1999@gmail.com>
2024-03-14 14:55:49 +0530

    class file related to the SourceFileConcept

---
+++ src/SourceFileConcepts.java
@@ -0,0 +1,33 @@
+
+//Public class
+public class SourceFileConcepts {
+
+    //Main method
+    public  static void main(String[] args) {
+
+
+   }
+
+}
+
+//Class without public modifier or default class
+class DefaultClass {
+
+
+}
+
+/*
+
+1. If the source file contain public class, then the source file name should match with the
+   public class name
+
+2. If there are no public classes in the source file , then there is no issue even if the
+   source file name is not matched with the class name
+
+3. Only one public class is allowed per source file
+
+4. Make sure that all the programs written by the end user inside the class body
+
+5. There will be a main method, from where execution of program starts
+
+ */
