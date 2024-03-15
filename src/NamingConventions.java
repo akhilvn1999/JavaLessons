2a35a30d9b48951b4617e47a4cd9f7eba6b1df5e
akhilvn1999 <akhilvnair1999@gmail.com>
2024-03-15 16:45:39 +0530

    java source file related to the different naming conventions in java

---
+++ src/NamingConventions.java
@@ -0,0 +1,30 @@
+
+//Lesson 3
+
+//NamingConventions is the class name, here N and C capitalized
+public class NamingConventions {
+
+
+ // localMethod is the method name, here starting letter l is lowercase after that M is capitalized
+public void localMethod(){
+
+    // testVariable is the variable name,here starting letter t is lowercase after that V is capitalized
+   int testVariable ;
+
+   // CONSTANT is the constant variable name, here all the words are in capital letters
+   final int CONSTANT ;
+
+}
+
+}
+
+
+/**
+ * Package name should be in lower case for example : com.packages
+ * Class name should be in mixed case. first letter of the each internal word should be capitalized. For example : NamingConventions
+ * Interface naming should be like class
+ * Method name start with lower case, then each internal word should be capitalized.For example : localMethod
+ * Variable name starts with lower case, then each internal word capitalized.For example : testVariable
+ * Variable name should not start with _ or $ sign. And white space not allowed in variable
+ * Constants name should be in capital letter
+ */
