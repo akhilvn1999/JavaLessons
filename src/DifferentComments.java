8a5c6bd5dc80e9c51df2788be6a4eb59017482df
akhilvn1999 <akhilvnair1999@gmail.com>
2024-03-15 12:31:27 +0530

    java source file related to the different comments in java

---
+++ src/DifferentComments.java
@@ -0,0 +1,26 @@
+public class DifferentComments {
+
+    // This is a single line comment. This comment is only applicable for line 3
+
+
+    /*Comments starts....
+
+       This is a multiline comment
+       This comment is only applicable with in these opening and closing
+
+   Comment Ends...*/
+
+
+    /**Documentation comment starts...
+
+     * Can use different types of tag to specify parameter like @author,@exception....
+     *This type of comment is used generally when writing code for a project/software package, since it helps to generate a documentation page for reference, which can be used for getting information about methods present, its parameters, etc.
+
+     Documentation comment ends...*/
+
+
+
+
+
+
+}
