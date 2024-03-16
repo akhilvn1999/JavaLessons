7bab0e5be4161cd285399d1b116374ee763f02aa
akhilvn1999 <akhilvnair1999@gmail.com>
2024-03-16 10:53:25 +0530

    java source file related to the data types in java

---
+++ src/DataTypes.java
@@ -0,0 +1,99 @@
+public class DataTypes {
+
+// Boolean data type : Store true or false values
+boolean a = true;
+
+/**
+
+ * default value is false
+ * size 1 bit means 0 or 1
+ * Range of values accepting : true or false
+
+ */
+
+// Byte data type : Store whole numbers
+byte b = 125;
+
+/**
+
+ * default value is 0
+ * size 8 byte
+ * Range of values accepting : -128 to 127
+
+ */
+
+//Char data type : Stores a single character/letter or ASCII values / Unicode values
+char c = 'a';
+
+/**
+
+ * default value is /u000
+ * size 2 byte
+ * Range of values accepting : 0 to 65535 ( UTF-16 or unicode standard )
+
+ */
+
+//Short data type : Store whole numbers
+short d = 20000;
+
+/**
+
+ * Default value is 0
+ * size is 2 byte
+ * Range of values accepting : -32,768 to 32,767
+
+ */
+
+
+//Int data type : Store whole numbers
+int e = 50000;
+
+/**
+
+ * Deafault value is 0
+ * Size is 4 byte
+ * Range of values accepting : around - 2 billion to 2 billion values
+
+ */
+
+
+//Long data type : Store whole numbers
+long f = 2147483648L;
+
+/**
+
+ * Default value is 0
+ * Size is 8 byte
+ * Range of values accepting : around -9 quintillion to 9 quintillion
+
+ */
+
+//Float data type : Store fractional numbers
+float g = 25.9012674F;
+
+/**
+
+ * Default value is 0.0F
+ * size is 4 byte
+ * it will display upto 7 digits after the decimal point
+
+ */
+
+//Double data type : Store fractional numbers
+double h = 34.12345678934;
+
+/**
+
+ * default value is 0.0
+ * Size is 8 byte
+ * it will display upto 15 digits after the decimal point
+
+ */
+
+/* -- These are the 8 primitive data types--------------*/
+
+// Non primitive data types : String(UTF-16),Arrays and Class
+
+}
+
+
